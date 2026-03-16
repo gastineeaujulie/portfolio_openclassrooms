b9d84cd3520f41752e90c57303426825ae3bdd07
gastineeaujulie <gastineeaujulie@gmail.com>
2026-03-16 15:29:03 +0100

    création des routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
-// import { useState } from 'react'
+import { Routes, Route } from 'react-router-dom';
+import Home from './sections/Home';
+import Projects from './sections/Projects';
+import Contact from './sections/Contact';
 
-import './App.css'
+import './App.css';
 
 function App() {
-  // const [count, setCount] = useState(0)
-
-  return <h1>Portfolio Julie Gastineau</h1>
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/projects" element={<Projects />} />
+      <Route path="/contact" element={<Contact />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
