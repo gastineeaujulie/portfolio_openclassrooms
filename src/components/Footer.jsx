3bffcf7a21e0a872423b2d2ef5d6c79734d4e27d
gastineeaujulie <gastineeaujulie@gmail.com>
2026-03-21 11:15:50 +0100

    ajout du responsive

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,8 @@ import '../style/components/Footer.scss';
 
 export default function Footer() {
   return (
-    <div className="footer">
+    <footer className="footer">
       <p>© 2026 Julie Gastineau — Tous droits réservés</p>
-    </div>
+    </footer>
   );
 }
